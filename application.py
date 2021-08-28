ff21d7f168a38a7ab404bf44442ea27e3c10d99f
Nabeel Shuaib <49292320+TemporarySolace@users.noreply.github.com>
2021-08-28 17:37:17 -0500

    Update application.py

--- application.py
+++ application.py
@@ -4,7 +4,7 @@ from flask import Flask, request, Response
 from flask_restful import Resource,Api
 import json
 
-app = Flask(__name__)
+application = app = Flask(__name__)
 api=Api(app)
 class Items(Resource):
     def get(self):
