eaf087367b7f9f3e2c70b6d03c8587e9ae1c08b0
Mohamed24444 <87099231+Mohamed24444@users.noreply.github.com>
2021-08-31 15:41:08 -0400

    Update helper.py

--- helper.py
+++ helper.py
@@ -73,3 +73,15 @@ def delete_item(item):
     except Exception as e:
         print('Error: ', e)
         return None
+def del_all():
+    try:
+        conn = sqlite3.connect(DB_PATH)
+        c = conn.cursor()
+        c.execute('select * from items')
+        rows = c.fetchall()
+        c.execute('delete from items')
+        conn.commit()
+        return { "del count": len(rows), "items": rows }
+    except Exception as e:
+        print('Error: ', e)
+        return None
